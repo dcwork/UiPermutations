51289d0362523d586bd32687bb657fd68507930c
Daniel Charles <daniel.charles@schneider-electric.com>
2021-03-16 13:23:31 -0700

    Initial commit

--- UwpApp/Properties/AssemblyInfo.cs
+++ UwpApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UwpLib")]
+[assembly: AssemblyTitle("UwpApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UwpLib")]
+[assembly: AssemblyProduct("UwpApp")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
